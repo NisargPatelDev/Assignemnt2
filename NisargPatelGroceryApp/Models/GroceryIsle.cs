fd8a960485d8e37487bad504dd3634592e213c52
Nisarg Patel <nisargpateldev@gmail.com>
2022-04-03 16:43:21 -0400

    Add files via upload

---
+++ NisargPatelGroceryApp/Models/GroceryIsle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NisargPatelGroceryApp.Models
+{
+    public class GroceryIsle
+    {
+        public List<FoodItem> FoodItems { get; set; }
+        public string IsleName { get; set; }
+        public Guid IsleNumber { get; set; }
+        public GroceryIsle()
+        {
+        }
+    }
+}
